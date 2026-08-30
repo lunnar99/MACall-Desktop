903002ce9e6995eb3652cc1b7864b289d6405538
lunnar99 <120673468+lunnar99@users.noreply.github.com>
2026-08-30 17:22:48 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    getDesktopSources: () => ipcRenderer.invoke('get-desktop-sources')
+    getDesktopSources: () => ipcRenderer.invoke('get-desktop-sources'),
+    getAppVersion: () => ipcRenderer.invoke('get-app-version') // NOVA LINHA
 });
