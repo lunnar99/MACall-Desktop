93e08b555dab5c4268d4131988fed07200712aac
lunnar99 <120673468+lunnar99@users.noreply.github.com>
2026-08-30 19:01:15 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,8 @@ contextBridge.exposeInMainWorld('electronAPI', {
     getDesktopSources: () => ipcRenderer.invoke('get-desktop-sources'),
     getAppVersion: () => ipcRenderer.invoke('get-app-version'),
     onUpdateReady: (callback) => ipcRenderer.on('update_ready', () => callback()),
-    onUpdaterStatus: (callback) => ipcRenderer.on('updater_status', (_event, msg) => callback(msg)), // NOVA LINHA
-    restartApp: () => ipcRenderer.send('restart_app')
+    onUpdaterProgress: (callback) => ipcRenderer.on('updater_progress', (_event, percent) => callback(percent)),
+    restartApp: () => ipcRenderer.send('restart_app'),
+    setGlobalMuteShortcut: (shortcut) => ipcRenderer.send('set_global_mute_shortcut', shortcut),
+    onToggleMuteGlobal: (callback) => ipcRenderer.on('toggle_mute_global', () => callback())
 });
